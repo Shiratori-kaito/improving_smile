c27c8ef46cb51b2182f43fb54a2b72d17cb6e1b0
Shiratori-kaito <sssffuv0809@gmail.com>
2024-01-15 13:28:59 +0900

    カメラ表示領域の調整(4)

--- app/javascript/camera/LoadingIndicator.jsx
+++ app/javascript/camera/LoadingIndicator.jsx
@@ -16,7 +16,7 @@ export const LoadingIndicator = () => {
   return loading
     ? (
     <>
-      <div className='flex item-center justify-center z-40 absolute top-[23%] lg:top-[4%] left-[2%] lg:left-[25%] h-[400px] lg:h-[550px] w-[350px] lg:w-[700px] bg-custom-bg  bg-cover animate-bg-slide'>
+      <div className='flex item-center justify-center z-40 absolute top-[23%] lg:top-[4%] left-[2%] lg:left-[25%] h-[500px] lg:h-[550px] w-[350px] lg:w-[700px] bg-custom-bg  bg-cover animate-bg-slide'>
         <div className="z-40 animate-spin h-[80px] lg:h-[120px] w-[80px] lg:w-[120px] border-[4px] lg:border-[5px] border-blue-900  rounded-full border-t-transparent absolute top-[90px] lg:top-[200px]"></div>
         <h2 className='text-xl lg:text-3xl text-blue-900 absolute top-[120px] lg:top-[240px]'>Loading</h2>
       </div>
