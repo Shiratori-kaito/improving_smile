4b0546e771d003c18201514e8871f63591cf50fd
Shiratori-kaito <sssffuv0809@gmail.com>
2024-02-08 16:47:00 +0900

    フォロワー表示のカラー変更

--- app/javascript/user/FollowerUsers.jsx
+++ app/javascript/user/FollowerUsers.jsx
@@ -9,7 +9,7 @@ export const FollowerUsers = ({ followers }) => {
   return (
     <div className="flex flex-col absolute top-[45%] left-[45%] z-20 ">
       {flatFollowers.map((follower) => (
-        <div key={follower.id} className="bg-yellow-200 border-2 border-gray-500 rounded-lg shadow p-4  flex items-center space-x-4">
+        <div key={follower.id} className="bg-red-200 border-2 border-gray-500 rounded-lg shadow p-4  flex items-center space-x-4">
           {/* 通常のaタグを使ってリンクを作成 */}
           <a href={`/users/${follower.id}`} className="flex items-center space-x-4">
             <img
